9d8fa3d26071ff8f418202e095d396b0436ad0c3
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-15 22:19:29 +0900

    Create No.1002.cpp

---
+++ AScode/Page.1/No.1002/No.1002.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+main(int argc, char** argv){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int A, B; cin >> A >> B;
+    cout << A + B;
+return 0; }
