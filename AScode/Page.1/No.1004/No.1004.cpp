a7a78bd0c0aeedc5d372b74ced569b2d57a26645
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-16 01:12:40 +0900

    Create No.1004.cpp

---
+++ AScode/Page.1/No.1004/No.1004.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int TC; cin >> TC;
+    while (TC--) {
+        int T; cin >> T;
+        int A, B, C; cin >> A >> B >> C;
+        for (int i = 1; i <= T; i++) {
+            if (i % A == 0) { cout << "A"; }
+            if (i % B == 0) { cout << "B"; }
+            if (i % C == 0) { cout << "C"; }
+        } cout << '\n';
+    }
+return 0; }
