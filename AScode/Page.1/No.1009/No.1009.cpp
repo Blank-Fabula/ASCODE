0b403d2bb5ed4a53aa469bc8bd5070377605fc3b
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-16 04:39:39 +0900

    Create No.1009.cpp

---
+++ AScode/Page.1/No.1009/No.1009.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    cin.tie(NULL); cout.tie(NULL);
+    int T; cin >> T;
+    while (T--) {
+        char alpha; cin >> alpha;
+        if (isupper(alpha)) { cout << char(tolower(alpha));
+        } else { cout << char(toupper(alpha)); }
+        cout << '\n';
+    }
+return 0; }
