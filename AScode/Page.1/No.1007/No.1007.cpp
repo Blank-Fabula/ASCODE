47aedf1771ee67f875c638cbed49fc675c30d390
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-16 03:44:26 +0900

    Create No.1007.cpp

---
+++ AScode/Page.1/No.1007/No.1007.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    cin.tie(NULL); cout.tie(NULL);
+	int X, Y, Z; cin >> X >> Y >> Z;
+	cout << (X + Y) % Z;
+return 0; }
