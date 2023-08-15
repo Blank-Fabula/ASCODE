179279207e88eba09221916c51ece665eb4695b6
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-16 04:13:22 +0900

    Create No.1008.c

---
+++ AScode/Page.1/No.1008/No.1008.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    float a, b, c, d, e, f; scanf("%f %f %f %f %f %f",&a, &b, &c, &d, &e, &f);
+    printf("%.4f", (a + b + c + d + e + f) / 6);
+return 0; }
