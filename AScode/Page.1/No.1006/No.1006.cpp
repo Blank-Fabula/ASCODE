71209cba8aabdc75b16f3e4d719633f3cf58785b
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-16 03:32:10 +0900

    Update No.1006.cpp

--- AScode/Page.1/No.1006/No.1006.cpp
+++ AScode/Page.1/No.1006/No.1006.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(int argc, char** argv) {
-    int T; cin >> T; getchar();
+    int T; cin >> T; cin.ignore();
     while (T--) {
         int alphaCnt[26] = { 0 };
-        for (; ;) {
-            char alpha = getchar();
-            if (alpha == '\n' || alpha == EOF) { break; }
-            alphaCnt[alpha - 'a']++;
-        }
+        string alpha; getline(cin, alpha);
+        for (int i = 0; i < alpha.size(); i++) { alphaCnt[alpha[i] - 'a']++; }
         for (int i = 0; i < 26; i++) {
             if (alphaCnt[i]) { cout << char(i + 'a') << ":" << alphaCnt[i] << '\n'; }
         } cout << '\n';
