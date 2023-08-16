533d3e4d9106b6719be674477008745402e1c79b
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-17 02:19:04 +0900

    Create No.1010.cpp

---
+++ AScode/Page.1/No.1010/No.1010.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    int X, Y; cin >> X >> Y;
+    cout << X << ", " << Y << ", ";
+    cout << X * Y / 2;
+return 0; }
