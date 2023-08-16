742d73784ed10dfff23509b55a0f21562fe1401e
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-17 07:38:29 +0900

    Update No.1013.cpp

--- AScode/Page.1/No.1013/No.1013.cpp
+++ AScode/Page.1/No.1013/No.1013.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
-#include <string>
-using namespace std;
 
-int main(int argc, char** argv) {
-	int T; cin >> T;
-	while (T--) {
-		string id; cin >> id;
-		cout << id[2] << id[3];
-		cout << '\n';
-	}
-return 0; }
