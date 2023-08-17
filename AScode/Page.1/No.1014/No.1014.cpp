e2b7df2a091c79c8450ab3dffac8284484de2516
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-17 09:53:40 +0900

    Create No.1014.cpp

---
+++ AScode/Page.1/No.1014/No.1014.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	int sum = 0, day = 1;
+	for (int num; cin >> num; day++) {
+		sum += num;
+		int c; c = cin.get();
+		if (c == EOF) { break; }
+	} cout << sum / day;
+return 0; }
