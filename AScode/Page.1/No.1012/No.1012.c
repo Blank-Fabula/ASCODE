971b351e1848c9d99ea102535607aa3797d68cd9
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-17 23:16:30 +0900

    Update No.1012.c

--- AScode/Page.1/No.1012/No.1012.c
+++ AScode/Page.1/No.1012/No.1012.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+#define MIN 60
+#define HOUR (MIN * 60)
+#define DAY (HOUR * 24)
+
+int main(int argc, char** argv) {
+	int T; scanf("%d", &T);
+	while (T--) {
+		unsigned int second; scanf("%u", &second);
+		printf("%u day : ", second / DAY);
+		printf("%u hour : ", second % DAY / HOUR);
+		printf("%u min : ", second % HOUR / MIN);
+		printf("%u sec", second % MIN);
+		printf("\n");
+	}
+return 0; }
