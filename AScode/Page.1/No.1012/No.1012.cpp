8468f1f6d78304a31ff94423b7668220a57f067f
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-17 23:14:53 +0900

    Update No.1012.cpp

--- AScode/Page.1/No.1012/No.1012.cpp
+++ AScode/Page.1/No.1012/No.1012.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 
-#define min 60
-#define hour (min * 60)
-#define day (hour * 24)
+#define MIN 60
+#define HOUR (MIN * 60)
+#define DAY (HOUR * 24)
+
 int main(int argc, char** argv) {
 	int T; cin >> T;
 	while (T--) {
 		unsigned int second; cin >> second;
-		cout << second / day << " day : ";
-		cout << second % day / hour << " hour : ";
-		cout << second % hour / min << " min : ";
-		cout << second % min << " sec" << '\n';
+		cout << second / DAY << " day : ";
+		cout << second % DAY / HOUR << " hour : ";
+		cout << second % HOUR / MIN << " min : ";
+		cout << second % MIN << " sec" << '\n';
 	}
 return 0; }
