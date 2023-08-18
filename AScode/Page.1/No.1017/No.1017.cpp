59d740a9f4f2fd4011f09cc94f98b9472938f69f
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-18 23:43:35 +0900

    Create No.1017.cpp

---
+++ AScode/Page.1/No.1017/No.1017.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	int N; cin >> N;
+	for (int i = 0; i < N; i++) {
+		int num; cin >> num;
+		cout << num << '\n';
+	}
+return 0; }
