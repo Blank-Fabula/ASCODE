9dc3a9fd336a96475373647091bcc220bd3c7c6b
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-18 23:43:20 +0900

    Create No.1017.c

---
+++ AScode/Page.1/No.1017/No.1017.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int N; scanf("%d", &N);
+    while (N--) {
+        int num; scanf("%d", &num);
+        printf("%d\n", num);
+    }
+return 0; }
