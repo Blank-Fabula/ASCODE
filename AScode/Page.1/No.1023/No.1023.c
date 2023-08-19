6c3e2dcaca9a30977f3384b916169782c85218f2
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-19 15:18:24 +0900

    Update No.1023.c

--- AScode/Page.1/No.1023/No.1023.c
+++ AScode/Page.1/No.1023/No.1023.c
@@ -5,13 +5,12 @@ int main(int argc, char** argv) {
     int TC; scanf("%d", &TC);
     while (TC--) {
         int N; scanf("%d", &N);
-        int n = N / 2;
         for (int i = 0; i < N; i++) { int cnt = N - N / 2 - 1;
-            for (int j = 0; j < N; j++) { printf("%d", 1 + ( i > n
+            for (int j = 0; j < N; j++) { printf("%d", 1 + ( i > N / 2
                 ? (j < abs(i - N + 1) ? cnt--
                     : (j >= N - abs(i - N + 1) ? ++cnt : cnt))
                 : (j < i ? cnt--
-                    : (j >= N-i ? ++cnt : cnt))));
+                    : (j >= N - i ? ++cnt : cnt))));
             } printf("\n");
         }
     }
